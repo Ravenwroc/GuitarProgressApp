66ecea6c1a3be2f0c4da54a597c80ea1b5e055a8
Marcin Guzek <ravenwroc@interia.pl>
2019-07-08 17:52:55 +0200

    Updates

---
+++ precache-manifest.eebc9b4b70705f2b798299348117bdcf.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8745da530b104309b95e013dd116fa95",
+    "url": "/GuitarProgressApp/index.html"
+  },
+  {
+    "revision": "1d9b55b48faeb2be5cfe",
+    "url": "/GuitarProgressApp/static/css/main.6cfcb289.chunk.css"
+  },
+  {
+    "revision": "1e8f8b6d73b84de5a027",
+    "url": "/GuitarProgressApp/static/js/2.a20d1eb9.chunk.js"
+  },
+  {
+    "revision": "1d9b55b48faeb2be5cfe",
+    "url": "/GuitarProgressApp/static/js/main.10d4662e.chunk.js"
+  },
+  {
+    "revision": "e0fcaabf1660079b25c4",
+    "url": "/GuitarProgressApp/static/js/runtime~main.50b43186.js"
+  },
+  {
+    "revision": "4a57a86a5e9e6ef95f5d4f8d576fd646",
+    "url": "/GuitarProgressApp/static/media/guitar.4a57a86a.jpg"
+  },
+  {
+    "revision": "24d17edf02b5f0208c6688c9957132c3",
+    "url": "/GuitarProgressApp/static/media/logo.24d17edf.svg"
+  }
+]);
